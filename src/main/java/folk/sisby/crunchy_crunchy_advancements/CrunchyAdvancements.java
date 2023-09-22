579a8586b1b66479121448e7f59caac60e8f7fae
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-09-22 14:21:48 +1000

    new versioning scheme, ditch api dep

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/CrunchyAdvancements.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/CrunchyAdvancements.java
@@ -1,18 +1,12 @@
 package folk.sisby.crunchy_crunchy_advancements;
 
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("deprecation")
-public class CrunchyAdvancements implements ModInitializer {
+public class CrunchyAdvancements {
 	public static final String ID = "crunchy_crunchy_advancements";
 	public static final Logger CRUNCHY_LOGGER = LoggerFactory.getLogger("Crunchy Crunchy Advancements");
 	public static final CrunchyConfig CONFIG = CrunchyConfig.createToml(FabricLoader.getInstance().getConfigDir(), ID, "config", CrunchyConfig.class);
-
-	@Override
-	public void onInitialize() {
-		CRUNCHY_LOGGER.info("[Crunchy Crunchy Advancements] Initialised!");
-	}
 }
--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/ScreenMixin.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/ScreenMixin.java
@@ -1,25 +1,32 @@
 package folk.sisby.crunchy_crunchy_advancements.mixin.client;
 
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
-import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.AbstractParentElement;
+import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @SuppressWarnings("deprecation")
 @Mixin(Screen.class)
 public abstract class ScreenMixin extends AbstractParentElement {
 	@Shadow
 	public int width;
+
+	@Shadow
+	@Final
+	private List<Drawable> drawables;
 
 	private static boolean buttonMatchesKey(ClickableWidget button, String key) {
 		Text buttonMessage = button.getMessage();
@@ -47,13 +54,15 @@ public abstract class ScreenMixin extends AbstractParentElement {
 		if ((Object) this instanceof GameMenuScreen) {
 			if (CrunchyAdvancements.CONFIG.removeAdvancementsButton) {
 				final int buttonWidth = 204;
-				for (ClickableWidget button : Screens.getButtons((Screen) (Object) this)) {
-					if (buttonMatchesKey(button, "gui.advancements")) {
-						button.visible = false;
-					}
-					if (buttonMatchesKey(button, "gui.stats")) {
-						button.setWidth(buttonWidth);
-						button.x = this.width / 2 - buttonWidth / 2;
+				for (Drawable element : this.drawables) {
+					if (element instanceof ClickableWidget button) {
+						if (buttonMatchesKey(button, "gui.advancements")) {
+							button.visible = false;
+						}
+						if (buttonMatchesKey(button, "gui.stats")) {
+							button.setWidth(buttonWidth);
+							button.x = this.width / 2 - buttonWidth / 2;
+						}
 					}
 				}
 			}
