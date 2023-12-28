ecce120d9544cacc930493a53f64737c6cc906e8
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-12-28 17:16:26 +1100

    update to 1.20.2

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/PlayerAdvancementTrackerMixin.java
@@ -1,19 +1,19 @@
 package folk.sisby.crunchy_crunchy_advancements.mixin;
 
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
 import net.minecraft.advancement.PlayerAdvancementTracker;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Optional;
+import java.util.function.Consumer;
 
 @Mixin(PlayerAdvancementTracker.class)
 public abstract class PlayerAdvancementTrackerMixin {
-	@Redirect(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
-	public void grantCriterionBroadcastRemover(PlayerManager instance, Text text, boolean bl) {
-		if (!CrunchyAdvancements.CONFIG.preventAdvancementBroadcasts) {
-			instance.broadcast(text, bl);
-		}
+	@WrapWithCondition(method = "grantCriterion", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V"))
+	public boolean grantCriterionBroadcastRemover(Optional<Object> instance, Consumer<Object> action) {
+		return !CrunchyAdvancements.CONFIG.preventAdvancementBroadcasts;
 	}
 }
+
--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/ServerAdvancementLoaderMixin.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/ServerAdvancementLoaderMixin.java
@@ -2,29 +2,30 @@ package folk.sisby.crunchy_crunchy_advancements.mixin;
 
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyConfig;
-import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.server.ServerAdvancementLoader;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.function.Predicate;
 
 @Mixin(ServerAdvancementLoader.class)
 public abstract class ServerAdvancementLoaderMixin {
-	@ModifyArg(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/AdvancementManager;load(Ljava/util/Map;)V"))
-	private Map<Identifier, Advancement.Builder> filterMap(Map<Identifier, Advancement.Builder> map) {
-		Predicate<Map.Entry<Identifier, Advancement.Builder>> namespace_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterNamespaces.contains(entry.getKey().getNamespace());
-		Predicate<Map.Entry<Identifier, Advancement.Builder>> path_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterPaths.stream().anyMatch(path -> entry.getKey().toString().startsWith(path));
-		Predicate<Map.Entry<Identifier, Advancement.Builder>> recipe_predicate = CrunchyAdvancements.CONFIG.filterRecipes ? (entry) -> entry.getValue().getCriteria().containsKey("has_the_recipe") : (entry) -> false;
-		Predicate<Map.Entry<Identifier, Advancement.Builder>> filter_predicate = namespace_predicate.or(path_predicate).or(recipe_predicate);
+	@ModifyArg(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/AdvancementManager;addAll(Ljava/util/Collection;)V"))
+	private Collection<AdvancementEntry> filterMap(Collection<AdvancementEntry> immutableEntries) {
+		Collection<AdvancementEntry> entries = new ArrayList<>(immutableEntries);
+		Predicate<AdvancementEntry> namespace_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterNamespaces.contains(entry.id().getNamespace());
+		Predicate<AdvancementEntry> path_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterPaths.stream().anyMatch(path -> entry.id().toString().startsWith(path));
+		Predicate<AdvancementEntry> recipe_predicate = CrunchyAdvancements.CONFIG.filterRecipes ? (entry) -> entry.value().comp_1915().containsKey("has_the_recipe") : (entry) -> false;
+		Predicate<AdvancementEntry> filter_predicate = namespace_predicate.or(path_predicate).or(recipe_predicate);
 		if (CrunchyAdvancements.CONFIG.filterMode.equals(CrunchyConfig.FilterMode.WHITELIST)) {
-			map.entrySet().removeIf(filter_predicate.negate());
+			entries.removeIf(filter_predicate.negate());
 		} else if (CrunchyAdvancements.CONFIG.filterMode.equals(CrunchyConfig.FilterMode.BLACKLIST)) {
-			map.entrySet().removeIf(filter_predicate);
+			entries.removeIf(filter_predicate);
 		}
-		return map;
+		return entries;
 	}
 }
