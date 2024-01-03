09849d7a9598373e62b195327675cb583157071b
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-01-03 11:41:11 +1100

    fix mixin on 1.20.2+

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/PlayerAdvancementTrackerMixin.java
@@ -2,16 +2,18 @@ package folk.sisby.crunchy_crunchy_advancements.mixin;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
+import net.minecraft.advancement.AdvancementDisplay;
 import net.minecraft.advancement.PlayerAdvancementTracker;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
 @Mixin(PlayerAdvancementTracker.class)
 public abstract class PlayerAdvancementTrackerMixin {
-	@WrapWithCondition(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
-	public boolean grantCriterionBroadcastRemover(PlayerManager instance, Text text, boolean bl) {
+	@WrapWithCondition(method = "grantCriterion", at = @At(value = "INVOKE", target = "Ljava/util/Optional;ifPresent(Ljava/util/function/Consumer;)V"))
+	public boolean grantCriterionBroadcastRemover(Optional<AdvancementDisplay> display, Consumer<? super Optional<AdvancementDisplay>> action) {
 		return !CrunchyAdvancements.CONFIG.preventAdvancementBroadcasts;
 	}
 }
