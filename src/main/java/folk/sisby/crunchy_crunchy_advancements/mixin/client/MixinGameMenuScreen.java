0888c035dbfe25c312048b2b59e0befe85121f8f
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-08-22 14:39:25 +1000

    port to 1.19

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/MixinGameMenuScreen.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/MixinGameMenuScreen.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,9 +23,9 @@ public abstract class MixinGameMenuScreen extends Screen {
 			Element advancements = null;
 			for (Element child : children()) {
 				if (child instanceof ButtonWidget) {
-					if (((ButtonWidget) child).getMessage().equals(new TranslatableText("gui.advancements"))) {
+					if (((ButtonWidget) child).getMessage().equals(Text.translatable("gui.advancements"))) {
 						advancements = child;
-					} else if (((ButtonWidget) child).getMessage().equals(new TranslatableText("gui.stats"))) {
+					} else if (((ButtonWidget) child).getMessage().equals(Text.translatable("gui.stats"))) {
 						((ButtonWidget) child).setWidth(204);
 						((ButtonWidget) child).x = width / 2 - 204 / 2;
 					}
