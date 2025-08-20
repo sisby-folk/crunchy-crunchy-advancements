7ea9a1a3a1a21dfe6b7b51a20b34de6322a7fd3b
sisby-folk <sleepingdragoninn@gmail.com>
2025-08-20 12:15:45 +1000

    fix filter mixin

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/ServerAdvancementLoaderMixin.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/ServerAdvancementLoaderMixin.java
@@ -1,31 +1,33 @@
 package folk.sisby.crunchy_crunchy_advancements.mixin;
 
+import com.google.common.collect.ImmutableMap;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyConfig;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.server.ServerAdvancementLoader;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
 
 @Mixin(ServerAdvancementLoader.class)
 public abstract class ServerAdvancementLoaderMixin {
-	@ModifyArg(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/AdvancementManager;addAll(Ljava/util/Collection;)V"))
-	private Collection<AdvancementEntry> filterMap(Collection<AdvancementEntry> immutableEntries) {
-		Collection<AdvancementEntry> entries = new ArrayList<>(immutableEntries);
+	@ModifyExpressionValue(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;buildOrThrow()Lcom/google/common/collect/ImmutableMap;"))
+	private ImmutableMap<Identifier, AdvancementEntry> filterMap(ImmutableMap<Identifier, AdvancementEntry> original) {
+		Map<Identifier, AdvancementEntry> map = new HashMap<>(original);
 		Predicate<AdvancementEntry> namespace_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterNamespaces.contains(entry.id().getNamespace());
 		Predicate<AdvancementEntry> path_predicate = (entry) -> CrunchyAdvancements.CONFIG.filterPaths.stream().anyMatch(path -> entry.id().toString().startsWith(path));
 		Predicate<AdvancementEntry> recipe_predicate = CrunchyAdvancements.CONFIG.filterRecipes ? (entry) -> entry.value().criteria().containsKey("has_the_recipe") : (entry) -> false;
 		Predicate<AdvancementEntry> filter_predicate = namespace_predicate.or(path_predicate).or(recipe_predicate);
 		if (CrunchyAdvancements.CONFIG.filterMode.equals(CrunchyConfig.FilterMode.WHITELIST)) {
-			entries.removeIf(filter_predicate.negate());
+			map.values().removeIf(filter_predicate.negate());
 		} else if (CrunchyAdvancements.CONFIG.filterMode.equals(CrunchyConfig.FilterMode.BLACKLIST)) {
-			entries.removeIf(filter_predicate);
+			map.values().removeIf(filter_predicate);
 		}
-		return entries;
+		return ImmutableMap.<Identifier, AdvancementEntry>builder().putAll(map).build();
 	}
 }
