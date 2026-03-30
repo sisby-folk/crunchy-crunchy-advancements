73daa789d383d7f4d9b7fd789ca871dfff12c9f6
sisby-folk <sleepingdragoninn@gmail.com>
2026-03-30 17:36:00 +1100

    port to 26.1

--- src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/MixinGameOptions.java
+++ src/main/java/folk/sisby/crunchy_crunchy_advancements/mixin/client/MixinGameOptions.java
@@ -1,17 +1,18 @@
 package folk.sisby.crunchy_crunchy_advancements.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import folk.sisby.crunchy_crunchy_advancements.CrunchyAdvancements;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.stream.Stream;
 
 @Mixin(GameOptions.class)
 public class MixinGameOptions {
@@ -19,15 +20,15 @@ public class MixinGameOptions {
 	@Final
 	public KeyBinding advancementsKey;
 
-	@ModifyArgs(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;<init>(Ljava/lang/String;ILjava/lang/String;)V"))
-	private void unbindAdvancementsKeybind(Args args) {
+	@ModifyArgs(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;<init>(Ljava/lang/String;ILnet/minecraft/client/option/KeyBinding$Category;)V"))
+	private static void unbindAdvancementsKeybind(Args args) {
 		if (CrunchyAdvancements.CONFIG.removeAdvancementsKeybind && args.get(0).equals("key.advancements")) {
 			args.set(1, InputUtil.UNKNOWN_KEY.getCode());
 		}
 	}
 
-	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/apache/commons/lang3/ArrayUtils;addAll([Ljava/lang/Object;[Ljava/lang/Object;)[Ljava/lang/Object;", remap = false), index = 0)
-	private Object[] removeAdvancementsKeybind(Object[] original) {
-		return CrunchyAdvancements.CONFIG.removeAdvancementsKeybind ? ArrayUtils.removeElement(original, advancementsKey) : original;
+	@ModifyExpressionValue(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;flatMap(Ljava/util/function/Function;)Ljava/util/stream/Stream;", remap = false))
+	private Stream<KeyBinding> removeAdvancementsKeybind(Stream<KeyBinding> original) {
+		return original.filter(k -> k != advancementsKey);
 	}
 }
